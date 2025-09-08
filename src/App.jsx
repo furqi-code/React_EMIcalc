a85484e34fa151fa249a04d5f3252d4e72b50ef4
Furqan <mdfurqanahmadda2@gmail.com>
2025-09-09 03:36:28 +0530

    Refactor EMI calculation logic and input handling in App components using single useState (better?? idk)

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,53 @@ import { Results } from "./components/results.jsx";
 import { useState } from "react";
 
 export function App() {
-  const [bill, setBill] = useState({
-    P: 0,
+  let initialValues = {
+    loanAmount: 0,
+    Rate_of_interest: 0,
+    loanTenure: 0, // years
+    principle: 0,
     monthlyEMI: 0,
     totalInterest: 0,
     totalAmount: 0,
-  });
+  };
+  const [emi, setEMI] = useState(initialValues);
 
-  const calculate = (p, emi, interest, amount) => {
-    setBill({
-      P: p,
-      monthlyEMI: emi,
-      totalInterest: interest,
-      totalAmount: amount,
+  const updateInput = (property, value) => {
+    setEMI({
+      ...emi,
+      [property]: value,
+    });
+  };
+
+  let handleCompute = () => {
+    if (emi.loanAmount < 100000) {
+      alert("Loan Amount must be 1 Lakh atleast");
+      setEMI(initialValues);
+      return;
+    }
+    if (emi.Rate_of_interest > 30 || emi.loanTenure > 30) {
+      alert("Rate of interest / Loan Tenure shouldn't be more than 30");
+      setEMI(initialValues);
+      return;
+    }
+
+    let P = emi.loanAmount;
+    let R = emi.Rate_of_interest / 12 / 100;
+    let N = emi.loanTenure * 12;
+    let monthlyEMI = 0 ;
+    if (R === 0) monthlyEMI = Math.round(P / N);
+    else
+      monthlyEMI = Math.round((P * R * Math.pow(1 + R, N)) / (Math.pow(1 + R, N) - 1));
+    let totalInterest = monthlyEMI * N - P;
+    let totalAmount = monthlyEMI * N;
+    console.log(P, R, N);
+    console.log(` EMI - ${monthlyEMI} \n interest - ${totalInterest} \n Total - ${totalAmount}`);
+    setEMI({
+      ...emi, // Keeping the current three input value
+      principle: P,
+      monthlyEMI: monthlyEMI,
+      totalInterest: totalInterest,
+      totalAmount: totalAmount,
     });
   };
 
@@ -25,14 +59,20 @@ export function App() {
       <Header></Header>
       <div className="container box">
         <div>
-          <Input cost={calculate}></Input>
+          <Input
+            cost={handleCompute}
+            updateInput={updateInput}
+            loanAmount={emi.loanAmount}
+            Rate_of_interest={emi.Rate_of_interest}
+            loanTenure={emi.loanTenure}
+          ></Input>
         </div>
         <div className="results-container">
           <Results
-            principle={bill.P}
-            emi={bill.monthlyEMI}
-            interest={bill.totalInterest}
-            amount={bill.totalAmount}
+            principle={emi.principle}
+            emi={emi.monthlyEMI}
+            interest={emi.totalInterest}
+            amount={emi.totalAmount}
           ></Results>
         </div>
       </div>
--- src/components/inputs.jsx
+++ src/components/inputs.jsx
@@ -1,42 +1,4 @@
-import { useState } from "react";
-
-export function Input({ cost }) {
-  let initialValues = {
-    loanAmount: 0,
-    Rate_of_interest: 0,
-    loanTenure: 0, // years
-  };
-  let [emi, setEMI] = useState(initialValues);
-
-  let handleCompute = () => {
-    if (emi.loanAmount < 100000) {
-      alert("Loan Amount must be 1 Lakh atleast");
-      setEMI(initialValues);
-      cost(0,0,0,0);
-      return;
-    }
-    if (emi.Rate_of_interest > 30 || emi.loanTenure > 30) {
-      alert("Rate of interest / Loan Tenure shouldn't be more than 30");
-      setEMI(initialValues);
-      cost(0,0,0,0);
-      return;
-    }
-
-    let P = emi.loanAmount;
-    let R = emi.Rate_of_interest / 12 / 100;
-    let N = emi.loanTenure * 12;
-    let monthlyEMI = 0 ;
-    if (R === 0)
-      monthlyEMI = Math.round(P / N);
-    else
-      monthlyEMI = Math.round((P * R * Math.pow(1 + R, N)) / (Math.pow(1 + R, N) - 1));
-    let totalInterest = monthlyEMI * N - P;
-    let totalAmount = monthlyEMI * N;
-    console.log(P, R, N);
-    console.log(` EMI - ${monthlyEMI} \n interest - ${totalInterest} \n Total - ${totalAmount}`);
-    cost(P, monthlyEMI, totalInterest, totalAmount);
-  };
-
+export function Input({ loanAmount, Rate_of_interest, loanTenure, cost, updateInput }) {
   return (
     <>
       <form>
@@ -52,9 +14,9 @@ export function Input({ cost }) {
               aria-label="number"
               id="loanAmount"
               name="loanAmount"
-              value={emi.loanAmount}
+              value={loanAmount}
               onChange={(event) =>
-                setEMI({ ...emi, loanAmount: event.target.value })
+                updateInput("loanAmount", event.target.value)
               }
             />
           </div>
@@ -69,9 +31,9 @@ export function Input({ cost }) {
               aria-label="number"
               id="roi"
               name="roi"
-              value={emi.Rate_of_interest}
+              value={Rate_of_interest}
               onChange={(event) =>
-                setEMI({ ...emi, Rate_of_interest: event.target.value })
+                updateInput("Rate_of_interest", event.target.value)
               }
             />
           </div>
@@ -86,9 +48,9 @@ export function Input({ cost }) {
               aria-label="number"
               id="loanTenure"
               name="loanTenure"
-              value={emi.loanTenure}
+              value={loanTenure}
               onChange={(event) =>
-                setEMI({ ...emi, loanTenure: event.target.value })
+                updateInput("loanTenure", event.target.value)
               }
             />
           </div>
@@ -96,7 +58,7 @@ export function Input({ cost }) {
             <button
               type="button"
               className="btn btn-outline-primary"
-              onClick={() => handleCompute()}
+              onClick={() => cost()}
             >
               Compute =
             </button>
